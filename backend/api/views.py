48e6c2bb938dac6bda09aa7c9b01a4df94e50741
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-06-17 09:01:12 -0300

    part8 done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,14 @@
 import json
 from django.http import JsonResponse
 
-
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    print(request.GET) # url query parameters (params)
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) #string of JSON data becomes a Python Dict
-    except:
-        pass
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers) # Same as old request.META
-    data['content_type'] = request.content_type
-    print(data)
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
