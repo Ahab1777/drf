1cb6f38a752a04df62b9aa77ca95416a4917aba0
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2025-06-17 08:15:23 -0300

    part7 done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,19 @@
+import json
 from django.http import JsonResponse
 
+
+
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, i'm a Django response!!!"})
+    # request -> HttpRequest -> Django
+    print(request.GET) # url query parameters (params)
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) #string of JSON data becomes a Python Dict
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers) # Same as old request.META
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
